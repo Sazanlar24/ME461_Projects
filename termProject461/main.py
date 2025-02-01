0e657c772e460387cd9952b249376dda93ee270b
huseyin-24 <06husokaya@gmail.com>
2025-02-01 13:53:54 +0300

    Add files via upload

---
+++ termProject461/main.py
@@ -0,0 +1,75 @@
+from design import Ui_MainWindow
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QMainWindow
+import socket
+
+HOST = '192.168.4.1'
+PORT = 8080
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.init_vars()
+
+        self.pushButton.clicked.connect(self.textHandler)
+        for i in range(1,len(self.buttons)+1): self.buttons[i].clicked.connect(self.clickHandler)
+        for button in self.radioButtons: button.clicked.connect(self.radioHandler)
+            
+    def sendPico(self, message:str):
+        client.sendall(message.encode()) 
+        response = client.recv(1024)
+        print("Received from Pico:", response.decode())
+        return response.decode()
+
+    def setDisplay(self, textOrImage:str, textOrPath:str):
+        self.scene.clear()
+        if textOrImage == 'text': 
+            self.scene.addText(textOrPath)
+        elif textOrImage == 'image': 
+            pixmap = QPixmap(textOrPath)
+            self.scene.addPixmap(pixmap)
+        else: print('Just provide str(text) or str(image) as first parameter.')
+
+        self.view = self.graphicsView.setScene(self.scene)
+
+    def radioHandler(self): 
+        button = self.sender().objectName()
+        print(f'{button} clicked!')
+
+    def clickHandler(self):
+        self.buttonColor(self.sender(),'green')
+        buttonName = self.sender().objectName()
+        print(f'{buttonName} clicked!')
+
+    def textHandler(self):
+        text = self.textEdit.toPlainText()
+        print(f'Text sent: {text}')
+        # Assign cell values to map for visual purposes
+        if text[:4] == 'path': 
+            path = text[5:]
+            cellValues = open(path, "r").read().split(',')
+            cellValues = [x.rstrip() for x in cellValues]
+            for i in range(1,len(self.buttons)+1): self.buttons[i].setText(cellValues[i-1])
+        
+        if  text[:4] == 'pico': 
+            message = text[5:]
+            self.sendPico(message)
+
+    def buttonColor(self, buttonObject:object, color:str):
+        buttonObject.setStyleSheet(f"background-color : {color}") 
+
+    def init_vars(self): 
+        self.progressBar.setValue(0)
+        for button in self.radioButtons: button.setChecked(False)
+        self.setDisplay('image', 'home.jpg')
+
+app = QApplication([])
+window = Window()
+
+window.show()
+app.exec()
+client.close()
